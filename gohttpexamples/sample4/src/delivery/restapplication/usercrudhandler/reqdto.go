4143455bfdda1e6d8d374285e0a220e9d21d2602
HemaC95 <funnybones695@gmail.com>
2019-02-12 01:51:25 +0530

    defined reqDTO for update & delete

---
+++ gohttpexamples/sample4/src/delivery/restapplication/usercrudhandler/reqdto.go
@@ -0,0 +1,18 @@
+package usercrudhandler
+
+type UserCreateReqDTO struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+}
+
+type UserUpdateDTO struct {
+	ID	  string `json :"id"`	
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Age       int    `json:"age"`
+}
+
+type UserDeleteDTO struct{
+	ID	string `json : "id"` 
+}
